0581f6e5f4a886cfae01183ae2381d69d1fa3c19
Yuinguyen2004 <129299230+Yuinguyen2004@users.noreply.github.com>
2025-07-07 22:15:52 +0700

    Recreate Backend with new NestJS project

--- Backend/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-const userRoutes = require('./routes/userRoutes');
-app.use('/api/users', userRoutes);
-
-module.exports = app;
