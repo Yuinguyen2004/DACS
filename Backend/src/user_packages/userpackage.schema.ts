e58d2e959b1dfacacfb142be37a755cb1713292b
Yuinguyen2004 <tonthongminh123@gmail.com>
2025-07-17 12:35:37 +0700

    Remove build and node_modules from repo

--- Backend/src/user_packages/userpackage.schema.ts
+++ Backend/src/user_packages/userpackage.schema.ts
@@ -1,15 +1,15 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+import { Package } from 'src/packages/package.schema';
+import { User } from 'src/users/user.schema';
 
 @Schema({ timestamps: true })
 export class UserPackage extends Document {
-  @Prop({ required: true, ref: 'User' })
-  user_id: string; // ref tới User
+  @Prop({ required: MongooseSchema.Types.ObjectId, ref: 'User' })
+  user_id: MongooseSchema.Types.ObjectId | User; // ref tới User
 
-  @Prop({required: true, ref: 'Package'})
-  package_id: string; // ref tới Package
-
-  @Prop({ required: true, default: 'active' })
-  status: string;
+  @Prop({ required: MongooseSchema.Types.ObjectId, ref: 'Package' })
+  package_id: MongooseSchema.Types.ObjectId | Package; // ref tới Package
 
   @Prop({ required: true })
   start_at: Date; // Ngày bắt đầu sử dụng gói
@@ -20,3 +20,5 @@ export class UserPackage extends Document {
   @Prop({ required: true })
   is_active: boolean; // Trạng thái gói, true nếu đang hoạt động, false nếu đã hết hạn
 }
+
+export const UserPackageSchema = SchemaFactory.createForClass(UserPackage);
